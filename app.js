be3c7a80b5a1747b8887ba391e243d8389573ca6
unoeufisenough <jonathan.colonieu@gmail.com>
2019-10-31 02:54:42 +0000

    mullet love

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
+var exphbs= require("express-handlebars");
+app.engine('.hbs', exphbs({// view engine setup
+  defaultLayout: 'player-layout',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  partialsDir: path.join(__dirname, "views/partials"),
+}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', '.hbs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Vaperooroo for my brooskerooroos' });
 });
 
+router.get('/about', function(req, res, next) {
+  //if you want to use a different layout for a page then use this:
+  //res.render('about', { layout:'main',title: 'Vaperooroo for my brooskerooroos' });
+  res.render('about', { title: 'Vaperooroo for my brooskerooroos' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Vaperooroo for my brooskerooroos' });
+});
+
+
 module.exports = router;
