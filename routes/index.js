67c9a1d6f7ec13af85a3679dd599dd30422c4403
unoeufisenough <jonathan.colonieu@gmail.com>
2019-12-09 19:42:02 +0000

    product pages and stylize

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,17 @@ router.get('/news', function(req, res, next) {
 });
 
 router.get('/product/smoknord', function(req, res, next) {
-  res.render('product', { pageType:'product-page', title: 'Vaperooroo: SMOK Nord' , productName: 'SMOK Nord', productPrice: '$40',productImage1:'https://www.electrictobacconist.com/images/smok-nord-p5831-15919_image.jpg' });
+  res.render('product', { pageType:'product-page', title: 'Vaperooroo: SMOK Nord' , productName: 'SMOK Nord', productPrice: '£40', productImage1:'https://www.electrictobacconist.com/images/smok-nord-p5831-15919_image.jpg', productImage2:'https://www.theelectroniccigarette.co.uk/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/s/m/smok-nord-vape-pod-full-black_1.jpg', productImage3:'https://cdn.shopify.com/s/files/1/0241/2241/products/Nord-white.jpg',
+  productImage4:'https://cdn.shopify.com/s/files/1/0241/2241/products/Nord-Gold.jpg?v=1570116752', productInfo:'The SMOK Nord is a versatile pod vape from SMOK. It has a lot in common with the Novo, while introducing some of its own unique advantages. The Nord utilizes 3 mL pods and different types of replaceable coil heads, suitable for both direct lung and mouth to lung vaping. It has an 1100 mAh battery capacity and an output of up to 15 watts. The Nord is currently offered in 13 different colors, featuring a cobra pattern or resin-style design.' });
+});
+router.get('/product/innokint18', function(req, res, next) {
+  res.render('product', { pageType:'product-page', title: 'Vaperooroo: Innokin Endura T18 II' , productName: 'Innokin Endura T18 II', productPrice: '£60',productImage1:'https://www.theelectroniccigarette.co.uk/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/n/innokin-endure-t18-ii-ecig-kit-colour-swatches_1.jpg' });
+});
+router.get('/product/aspirezeloskit', function(req, res, next) {
+  res.render('product', { pageType:'product-page', title: 'Vaperooroo: Aspire Zelos Kit' , productName: 'Aspire Zelos Kit', productPrice: '£40',productImage1:'https://www.theelectroniccigarette.co.uk/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/a/s/aspire-zelos-2.0-ecig-kit-swatches_1.jpg' });
+});
+router.get('/product/v2proseries3', function(req, res, next) {
+  res.render('product', { pageType:'product-page', title: 'Vaperooroo: V2 Pro Series 3' , productName: 'V2 Pro Series 3', productPrice: '£50',productImage1:'https://cdn11.bigcommerce.com/s-1h049t/images/stencil/1280x1280/products/78/852/vapour2-pro-series-3-all-colours-1280x1280__79090.1495185247.jpg?c=2?imbypass=on&imbypass=on' });
 });
 
 module.exports = router;
